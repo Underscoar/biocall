97db11e72aa252293ec300ba592647a49a0b9c6c
Arne Reijntjes <arne123@live.nl>
2020-04-15 15:03:01 +0200

    Added changing border color based on GSR

--- src/App.js
+++ src/App.js
@@ -10,21 +10,41 @@ class App extends Component {
     super();
     this.state = {
       response: false,
-      endpoint: "http://127.0.0.1:4001"
+      endpoint: "http://127.0.0.1:4001",
+      borderStyle: {
+        boxShadow: `0 0 20px 5px rgba(90, 255, 52, 0.75)`
+      }
     }
   }
 
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-    socket.on("testId", data => console.log(data))
+    socket.on("eSenseData", data => this.processESenseDate(data));
+  }
+
+  processESenseDate(data) {
+    console.log(data);
+    const bioSource = data[0];
+    const bioValue = parseFloat(data[1]);
+    if (bioSource === '/GSR') {
+      // Set the border color based on the Skin Response Value
+      if (bioValue <= 1) {this.setState({borderStyle: {boxShadow: '0 0 20px 5px rgba(0, 0, 255, 0.75)'}});}
+      else if (bioValue >= 3) {this.setState({borderStyle: {boxShadow: '0 0 20px 5px rgba(255, 0, 0, 0.75)'}});}
+      else {
+        let percOfThreeVal = bioValue/3;
+        let redVal = Math.floor(255*percOfThreeVal);
+        let blueVal = 255-redVal;
+        this.setState({borderStyle: {boxShadow: `0 0 20px 5px rgba(${redVal}, 0, ${blueVal}, 0.75)`}});
+      }
+    }
   }
 
   render() {
     return (
       <div className="App">
         <div className="jitsi-window">
-          <div class="jitsi-wrap border-green">
+          <div style={this.state.borderStyle} className="jitsi-wrap">
             <JitsiContainer />
           </div>
         </div>
